0489a5f36421da36fb9e313b7c287f4027edaf98
AltTheReal <145946205+AltTheReal@users.noreply.github.com>
2023-09-26 11:52:28 -0400

    Update adminmodule.lua

--- adminmodule.lua
+++ adminmodule.lua
@@ -48,3 +48,43 @@ local function unfreezePlayer(player)
         end
     end
 end
+
+-- Function to handle player chat and check for admin commands
+local function onPlayerChat(player, message)
+    if isAdmin(player) then
+        if message:lower():sub(1, 5) == ".kill" then
+            local playerNameToKill = message:sub(7)
+            local targetPlayer = Players:FindFirstChild(playerNameToKill)
+            killPlayer(targetPlayer)
+        elseif message:lower():sub(1, 5) == ".kick" then
+            local playerNameToKick = message:sub(7)
+            local targetPlayer = Players:FindFirstChild(playerNameToKick)
+            kickPlayer(player, targetPlayer)
+        elseif message:lower():sub(1, 7) == ".freeze" then
+            local playerNameToFreeze = message:sub(9)
+            local targetPlayer = Players:FindFirstChild(playerNameToFreeze)
+            freezePlayer(targetPlayer)
+        elseif message:lower():sub(1, 9) == ".unfreeze" then
+            local playerNameToUnfreeze = message:sub(11)
+            local targetPlayer = Players:FindFirstChild(playerNameToUnfreeze)
+            unfreezePlayer(targetPlayer)
+        -- Add more admin commands here
+        end
+    end
+end
+
+-- Listen for chat messages from all players
+for _, player in pairs(Players:GetPlayers()) do
+    player.Chatted:Connect(function(msg)
+        onPlayerChat(player, msg)
+    end)
+end
+
+-- Listen for new players and their chat messages
+Players.PlayerAdded:Connect(function(player)
+    player.Chatted:Connect(function(msg)
+        onPlayerChat(player, msg)
+    end)
+end)
+
+
