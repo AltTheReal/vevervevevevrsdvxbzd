23ea36b562dd6703003a1a5a061073603a65a469
AltTheReal <145946205+AltTheReal@users.noreply.github.com>
2023-12-14 19:01:33 -0500

    Create MobileScript.lua

---
+++ MobileScript.lua
@@ -0,0 +1,198 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+local Window = OrionLib:MakeWindow({Name = "kill arua", HidePremium = false, SaveConfig = false, IntroEnabled = true})
+--Services
+Workspace = game:GetService('Workspace')
+Players = game:GetService('Players')
+ReplicatedStorage = game:GetService('ReplicatedStorage')
+UserInputService = game:GetService('UserInputService')
+TweenService = game:GetService("TweenService")
+RunService = game:GetService('RunService')
+Lighting  = game:GetService('Lighting')
+VirtualUser = game:GetService("VirtualUser")
+HttpService = game:GetService('HttpService')
+TeleportService = game:GetService("TeleportService")
+
+--Globals
+LocalPlayer = Players.LocalPlayer
+Mouse = LocalPlayer:GetMouse()
+Camera = Workspace.CurrentCamera
+ALLITEMS = require(ReplicatedStorage:WaitForChild('References'):WaitForChild('SharedData'):WaitForChild('Items'))['Items']
+ALLITEMSTABLE = {}
+SWITCHEDITEMSTABLE = {}
+for id, index in pairs(ALLITEMS) do
+	ALLITEMSTABLE[index['id']] = index['Name']
+end
+for id, index in pairs(ALLITEMS) do
+	SWITCHEDITEMSTABLE[index['Name']] = index['id']
+end
+--RemoteEvents
+RemoteEvents = {
+    ToolAction = ReplicatedStorage:WaitForChild('References'):WaitForChild('Comm'):WaitForChild('Events'):WaitForChild('ToolAction');
+    InventoryInteraction =  ReplicatedStorage:WaitForChild("References"):WaitForChild("Comm"):WaitForChild("Events"):WaitForChild("InventoryInteraction");
+    UpdateStorageChest =  ReplicatedStorage:WaitForChild("References"):WaitForChild("Comm"):WaitForChild("Events"):WaitForChild("UpdateStorageChest");
+    SetSettings = ReplicatedStorage:WaitForChild("References"):WaitForChild("Comm"):WaitForChild("Events"):WaitForChild("SetSettings");
+    BuyWorldEvent =  ReplicatedStorage:WaitForChild("References"):WaitForChild("Comm"):WaitForChild("Events"):WaitForChild("BuyWorldEvent");
+    ItemInteracted =  ReplicatedStorage:WaitForChild("References"):WaitForChild("Comm"):WaitForChild("Events"):WaitForChild("ItemInteracted");
+    CraftItem =  ReplicatedStorage:WaitForChild("References"):WaitForChild("Comm"):WaitForChild("Events"):WaitForChild("CraftItem");
+    TradeTrader =  ReplicatedStorage:WaitForChild("References"):WaitForChild("Comm"):WaitForChild("Events"):WaitForChild("TradeTrader");
+    KeyDoor = ReplicatedStorage:WaitForChild("References"):WaitForChild("Comm"):WaitForChild("Events"):WaitForChild("KeyDoor");
+    Sonar =  ReplicatedStorage:WaitForChild('References'):WaitForChild('Comm'):WaitForChild('Events'):WaitForChild('Sonar');
+}
+--Important locals
+local message = require(LocalPlayer:WaitForChild('PlayerScripts'):WaitForChild('Main'):WaitForChild('Message'))
+local MyInventory = LocalPlayer:WaitForChild('PlayerGui'):FindFirstChild('Menus'):FindFirstChild('Inventory'):FindFirstChild('Inventory'):FindFirstChild('List')
+local Whitelist_table = {};
+local OpKillAuraTable = {};
+local realgameadmins = {849400193, 134488231, 146733116, 27865601}
+local MoonstoneSet = {363, 364, 365, 366}
+local ObsidianSet = {225, 226, 227, 228}
+local AllShields = {206, 207, 208, 209, 210, 211, 219, 235, 367, 379}
+local AllSwords = {173, 205, 230, 369, 255, 254, 253}
+local AllBows = {174, 197, 198, 199, 376}
+local AllBooks = {281, 282, 283, 284, 285, 286, 287, 296, 302, 303, 304, 305, 306, 307, 308, 309, 310, 311, 312, 313, 314, 315, 323, 362}
+local AllStaffs = {293, 292, 291, 290, 162, 289}
+
+--Safe Death Connections
+local TimeTped
+local TimeBetweenTps
+local TeleportHappened = false
+local SafeDeathHealthChecker = nil
+
+--Boss Death connections
+local AutoPickupOnObsidianDeath
+local AutoPickupOnZenLuckBossDeath
+local AutoPickupOnSpiritBossDeath
+local AutoPickuponLuckySlimeDeath
+local AutoPickupOnSkeletonDeath
+local AutoPickupOnOgreDeath
+local AutoPickupOnSquidDeath
+
+--Dupe locals
+local ItemIndexed
+local ItemIndexedNumber
+
+--Aimbot locals
+local CurrentlyLocked
+local Aiming = false
+
+--Quickspeed locals
+local OnOff = false
+local keydetected 
+
+--setup ac bypass
+--[[if not getgenv().bypassing then
+    getgenv().bypassing = true
+    local bypassac
+
+    bypassac = hookmetamethod(game, '__namecall', function(self, ...)
+        local args = {...}
+        if not checkcaller() and self == RemoteEvents['Sonar'] then
+            return nil
+        end
+        return bypassac(self, ...)
+    end) 
+end
+]]
+local Tab = Window:MakeTab({
+	Name = "main",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local Section = Tab:AddSection({
+	Name = "Stuff"
+
+Tab:AddToggle({
+    Name = 'kill arua',
+    Callback = function()
+        function KillAura()
+    while getgenv().configs.KillAura do
+        if getgenv().configs.KillAura then
+            task.wait()
+            local function GetClosestKAPlayer()
+                local range = 32
+                local closest
+                for _, plr in pairs(Players:GetPlayers()) do
+                    if plr ~= LocalPlayer then
+                        if not table.find(Whitelist_table, plr.Name) then
+                            if IsPlayerAlive(plr) and IsPlayerAlive(LocalPlayer) then
+                                local char = plr.Character
+                                if char:FindFirstChild("PlayerBillboard") and char:FindFirstChild('PlayerBillboard'):FindFirstChild('Title') and char:FindFirstChild('PlayerBillboard'):FindFirstChild('Title'):FindFirstChild('Icon') then
+                                    if char:FindFirstChild('PlayerBillboard'):FindFirstChild('Title'):FindFirstChild('Icon').BackgroundColor3 == Color3.fromRGB(80, 63, 47) then
+                                        local mypos = LocalPlayer.Character.HumanoidRootPart.Position
+                                        local plrpos = plr.Character.HumanoidRootPart.Position
+                                        local dist = (mypos - plrpos).magnitude
+                                        if dist < range then
+                                            range = dist
+                                            closest = plr.Character
+                                        end
+                                    else
+                                        if char:FindFirstChild('PlayerBillboard'):FindFirstChild('Title'):FindFirstChild('Icon').BackgroundColor3 ~= LocalPlayer.Character:FindFirstChild('PlayerBillboard'):FindFirstChild('Title'):FindFirstChild('Icon').BackgroundColor3 then
+                                            local mypos = LocalPlayer.Character.HumanoidRootPart.Position
+                                            local plrpos = plr.Character.HumanoidRootPart.Position
+                                            local dist = (mypos - plrpos).magnitude
+                                            if dist < range then
+                                                range = dist
+                                                closest = plr.Character
+                                            end
+                                        end
+                                    end
+                                end
+                            end
+                        end
+                    end
+                end
+                return closest
+            end
+            local NearestPlayer = GetClosestKAPlayer()
+            if NearestPlayer then
+                RemoteEvents['ToolAction']:FireServer(NearestPlayer)
+            end
+        end
+    end
+end 
+Tab:AddButton({
+    Name = 'Infinite Yield', 
+    Callback = function()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source', true))()
+    end
+})
+
+--[[Tab:AddButton({
+    Name = "AC Bypass",
+    Callback = function()
+        if not IsPlayerAlive(LocalPlayer) then
+            MakeAtlasNotification('Not alive', 'Maybe click the play button?', 5)
+            return
+        end
+        if IsPlayerAlive(LocalPlayer) then
+            local oldpos = LocalPlayer.Character.HumanoidRootPart.CFrame
+            local myroot = LocalPlayer.Character.HumanoidRootPart
+			for i=1, 100 do
+				myroot.CFrame = CFrame.new(7562, 221, 18946)
+            	RemoteEvents['Sonar']:FireServer()
+            	myroot.CFrame = oldpos
+			end
+            message.New('Positive', 'Bypassed AntiCheat')
+        end
+    end
+})
+]]
+Tab:AddButton({
+    Name = 'Stop data(dupe)',
+    Callback = function(Value)
+        RemoteEvents['SetSettings']:FireServer(Workspace)
+		OrionLibNotification('Data Stopped', 'Drop anything you want :)', 5)
+    end
+})
+
+Tab:AddButton({
+    Name = 'Rejoin Current Server',
+    Callback = function(Value)
+		OrionLibNotification('Rejoining', 'Rejoining the server', 1)
+        TeleportService:Teleport(game.PlaceId, LocalPlayer)
+    end
+})
+
+print("Loaded")
